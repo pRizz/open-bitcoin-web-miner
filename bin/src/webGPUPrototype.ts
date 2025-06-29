8132124a2f56ce24871f4eb965134ca863f1afed
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2025-06-28 20:09:51 -0500

    preparing to prototype webgpu testing in the node binary

---
+++ bin/src/webGPUPrototype.ts
@@ -0,0 +1,8 @@
+
+import { parentPort, workerData } from 'node:worker_threads';
+
+// webGPUPrototypeMain();
+
+export async function webGPUPrototypeMain() {
+  console.log('webGPUPrototypeMain');
+}
