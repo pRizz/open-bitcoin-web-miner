ba1e7f0c11a20cc719ef3b6efcc9fcd77eff208d
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2025-06-28 19:19:26 -0500

    Make script for updating all aws domains with iron st address

--- bin/src/index.ts
+++ bin/src/index.ts
@@ -4,71 +4,79 @@ import { Command } from 'commander';
 import { bytesToBuffer, doubleSha256, createBlockFromTemplate } from './RewardVerifier';
 import { calculateRequiredLeadingBinaryZeroes } from './miningCopy';
 import { readFileSync } from 'fs';
+// import { updateAWSDomainAddressesMain } from './updateAWSDomainAddresses';
 
 // Run with:
 // pnpm run bin:dev
 // Build to find typescript errors with:
 // pnpm run build:bin
 
-// const program = new Command();
+// await updateAWSDomainAddressesMain();
 
-// program
-//   .name('my-binary')
-//   .description('A CLI tool for your application')
-//   .version('1.0.0');
+function main() {
+  // const program = new Command();
 
-// program
-//   .command('hello')
-//   .description('Say hello')
-//   .action(() => {
-//     console.log('Hello from your binary!');
-//   });
+  // program
+  //   .name('my-binary')
+  //   .description('A CLI tool for your application')
+  //   .version('1.0.0');
 
-// program.parse(process.argv);
+  // program
+  //   .command('hello')
+  //   .description('Say hello')
+  //   .action(() => {
+  //     console.log('Hello from your binary!');
+  //   });
 
-// console.log(program.args);
-// console.log(program.opts());
-// console.log(program.commands);
-console.log('Running a CLI tool for your application' );
+  // program.parse(process.argv);
 
-const buffer = bytesToBuffer([1, 2, 3]);
-console.log(buffer);
+  // console.log(program.args);
+  // console.log(program.opts());
+  // console.log(program.commands);
+  console.log('Running a CLI tool for your application' );
 
-const stringToHash = "hello";
-const hash = doubleSha256(Buffer.from(stringToHash, 'utf8'));
-// <Buffer 95 95 c9 df 90 07 51 48 eb 06 86 03 65 df 33 58 4b 75 bf f7 82 a5 10 c6 cd 48 83 a4 19 83 3d 50>
-console.log(hash);
+  const buffer = bytesToBuffer([1, 2, 3]);
+  console.log(buffer);
 
-console.log(`calculateRequiredLeadingBinaryZeroes(1) (1 Hash): ${calculateRequiredLeadingBinaryZeroes(1)}`);
-console.log(`calculateRequiredLeadingBinaryZeroes(1_000) (1 kHashes): ${calculateRequiredLeadingBinaryZeroes(1_000)}`);
-console.log(`calculateRequiredLeadingBinaryZeroes(1_000_000) (1 MHashes): ${calculateRequiredLeadingBinaryZeroes(1_000_000)}`);
-console.log(`calculateRequiredLeadingBinaryZeroes(1_000_000_000) (1 GHashes): ${calculateRequiredLeadingBinaryZeroes(1_000_000_000)}`);
-console.log(`calculateRequiredLeadingBinaryZeroes(1_000_000_000_000) (1 THashes): ${calculateRequiredLeadingBinaryZeroes(1_000_000_000_000)}`);
-console.log(`calculateRequiredLeadingBinaryZeroes(1_000_000_000_000_000) (1 PHashes): ${calculateRequiredLeadingBinaryZeroes(1_000_000_000_000_000)}`);
+  const stringToHash = "hello";
+  const hash = doubleSha256(Buffer.from(stringToHash, 'utf8'));
+  // <Buffer 95 95 c9 df 90 07 51 48 eb 06 86 03 65 df 33 58 4b 75 bf f7 82 a5 10 c6 cd 48 83 a4 19 83 3d 50>
+  console.log(hash);
 
-// calculateRequiredLeadingBinaryZeroes(1) (1 Hash): 32
-// calculateRequiredLeadingBinaryZeroes(1_000) (1 kHashes): 41
-// calculateRequiredLeadingBinaryZeroes(1_000_000) (1 MHashes): 51
-// calculateRequiredLeadingBinaryZeroes(1_000_000_000) (1 GHashes): 61
-// calculateRequiredLeadingBinaryZeroes(1_000_000_000_000) (1 THashes): 71
-// calculateRequiredLeadingBinaryZeroes(1_000_000_000_000_000) (1 PHashes): 81
+  console.log(`calculateRequiredLeadingBinaryZeroes(1) (1 Hash): ${calculateRequiredLeadingBinaryZeroes(1)}`);
+  console.log(`calculateRequiredLeadingBinaryZeroes(1_000) (1 kHashes): ${calculateRequiredLeadingBinaryZeroes(1_000)}`);
+  console.log(`calculateRequiredLeadingBinaryZeroes(1_000_000) (1 MHashes): ${calculateRequiredLeadingBinaryZeroes(1_000_000)}`);
+  console.log(`calculateRequiredLeadingBinaryZeroes(1_000_000_000) (1 GHashes): ${calculateRequiredLeadingBinaryZeroes(1_000_000_000)}`);
+  console.log(`calculateRequiredLeadingBinaryZeroes(1_000_000_000_000) (1 THashes): ${calculateRequiredLeadingBinaryZeroes(1_000_000_000_000)}`);
+  console.log(`calculateRequiredLeadingBinaryZeroes(1_000_000_000_000_000) (1 PHashes): ${calculateRequiredLeadingBinaryZeroes(1_000_000_000_000_000)}`);
 
-// Print pwd
-console.log(`pwd: ${process.cwd()}`);
+  // calculateRequiredLeadingBinaryZeroes(1) (1 Hash): 32
+  // calculateRequiredLeadingBinaryZeroes(1_000) (1 kHashes): 41
+  // calculateRequiredLeadingBinaryZeroes(1_000_000) (1 MHashes): 51
+  // calculateRequiredLeadingBinaryZeroes(1_000_000_000) (1 GHashes): 61
+  // calculateRequiredLeadingBinaryZeroes(1_000_000_000_000) (1 THashes): 71
+  // calculateRequiredLeadingBinaryZeroes(1_000_000_000_000_000) (1 PHashes): 81
 
-const blockTemplateJSON = readFileSync('./bin/test-data/2025-04-16_23-01-16_block_template.json', 'utf8');
-const blockTemplate = JSON.parse(blockTemplateJSON);
-console.log(`blockTemplate: ${blockTemplate}`);
-console.log(`blockTemplate,`, blockTemplate);
+  // Print pwd
+  console.log(`pwd: ${process.cwd()}`);
 
-const coinbaseHex = readFileSync('./bin/test-data/2025-04-16_23-01-16_coinbaseHex.txt', 'utf8');
-console.log(`coinbaseHex: ${coinbaseHex}`);
-const coinbaseTxBuffer = Buffer.from(coinbaseHex, 'hex');
-console.log(`coinbaseTxBuffer: ${coinbaseTxBuffer}`);
+  const blockTemplateJSON = readFileSync('./bin/test-data/2025-04-16_23-01-16_block_template.json', 'utf8');
+  const blockTemplate = JSON.parse(blockTemplateJSON);
+  console.log(`blockTemplate: ${blockTemplate}`);
+  console.log(`blockTemplate,`, blockTemplate);
 
-// as hex string
-// const coinbaseTxHex = coinbaseTxBuffer.toString('hex');
-// console.log(`coinbaseTxHex: ${coinbaseTxHex}`);
+  const coinbaseHex = readFileSync('./bin/test-data/2025-04-16_23-01-16_coinbaseHex.txt', 'utf8');
+  console.log(`coinbaseHex: ${coinbaseHex}`);
+  const coinbaseTxBuffer = Buffer.from(coinbaseHex, 'hex');
+  console.log(`coinbaseTxBuffer: ${coinbaseTxBuffer}`);
 
-const block = createBlockFromTemplate(blockTemplate, coinbaseHex);
-// console.log(`block: ${block}`);
+  // as hex string
+  // const coinbaseTxHex = coinbaseTxBuffer.toString('hex');
+  // console.log(`coinbaseTxHex: ${coinbaseTxHex}`);
+
+  const block = createBlockFromTemplate(blockTemplate, coinbaseHex);
+  // console.log(`block: ${block}`);
+
+}
+
+main();
